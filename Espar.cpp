d8f138e274822f7548c79c69cde6e8f9d58f7a46
River6225 <krisriver62@gmail.com>
2025-06-17 00:14:57 -0600

    Programa terminado de momento

---
+++ Espar.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+/* Ejercicio #1: Escribe una función
+llamada esPar que acepte un número
+entero como parámetro y devuelva true
+si el número es par o false si es impar.
+Luego, en la función main(), solicita al
+usuario que ingrese un número, llama a
+la función esPar con este número como
+argumento y muestra un mensaje en la
+consola indicando si el número es par o
+impar. */
+
+bool esPar (int &evaluar){
+    bool par = false;
+
+    if (evaluar % 2 == 0){
+        par = true;
+    }
+    return par;
+}
+
+
+int main(){
+   
+    int respuesta;
+   do
+   {
+       int numero;
+       cout << " Numero a evaluar ";
+       cin >> numero;
+    
+    
+       // llamar a la funcion dentro de la decision para acelerar el proceso 
+       
+       if (esPar (numero)) // Si retorna true entra a la condicion si no entra a la parte falsa 
+       {
+           cout << "El numero: " << numero << " es par "; 
+       }else{
+           cout << "El numero: " << numero << " es impar ";
+       }
+      cout << "Desea evaluar otro numero? (1. Si || 2. No)";
+    } while (respuesta == 1);
+    
+
+
+
+    return 0;
+}
+
